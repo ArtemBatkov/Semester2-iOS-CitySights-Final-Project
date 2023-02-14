5f8e637251da94846566e69e221e6626ad86e550
ArtemBatkov <artembatkov2015@gmail.com>
2023-02-13 20:45:37 -0500

    Initial Commit

---
+++ CitiesSights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CitiesSights
+//
+//  Created by user233573 on 2/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
