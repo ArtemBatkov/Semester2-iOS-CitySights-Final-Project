3f44ea0e214683dda67bf82a832e37b73e015524
ArtemBatkov <artembatkov2015@gmail.com>
2023-03-02 10:16:55 -0500

    Branch #1Version 1.0Commit #2What's new: - This is the first, but very important commit. The initial functionality of the app was to test OpenTripMap API. That's why 2 text fields and 3 buttons were built. - Also, files were split into appropriate folders: WebServices were added to the Service folder, models are in Model, ViewController into ViewControllers, and so on.- These text fields are used to enter a city and country code, while buttons make requests to API and fetch the data. - Button 1. The button takes a city and a country code, makes a url, and also do a request. The result of the request turns into CityModel. The main property used is the coordinates section. These coordinates are the center of the circle.- Button 2. The button takes the city's coordinates and sets up them as the center of the circle. Also, the radius could be selected in the previous version, but now it is 30 km or 30, 000 meters. The more radius is the more sights can be captured. The result presents a list of sights turned into SightCollection. Basically, it has 3 main properties: xid, title, and coordinates of the sight. Xid is the most important property, it helps to know more information about the sight by sending a specific request, but is can be seen, that an object of SightCollection doesn't contain an image. The idea is to display a list of sights including an image. - Button 3. The button takes an object of SightCollection, and sends a request to fetch detailed information about the sight, including the image. Here is the main problem with the app. There are  2 issues, the first one is some sights don't have images, potential solution is to drop off the sight, or set up the default image. The second issue is that API has restrictions:  10 queries per second and 5,000 per month. To display data, the console is used, so, a sleep method may be fired to restrict the number of queries, but  UI will be laggy. A potential solution is to restrict ViewTable by the bottom button "LoadMore", and load every 10 new objects each time.

--- CitiesSights/ViewController.swift
+++ CitiesSights/ViewController.swift
@@ -7,13 +7,139 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CityTextField: UITextField!
+    
+    @IBOutlet weak var Button1_CityInfo: UIButton!
+    
+    
+    @IBOutlet weak var Button2_SightsInfo: UIButton!
+    
+    @IBOutlet weak var CountryCodeTextField: UITextField!
+    
+    @IBOutlet weak var Button3_DetailedSightesInfo: UIButton!
+    
+    public var MyCity: CityModel? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func Button1_CityInfo(_ sender: Any){
+        Task{
+            await Button1_Click()
+        }
+    }
+    
+    private func Button1_Click() async {
+        let name = CityTextField.text;
+        let code = CountryCodeTextField.text;
+        var url = "https://api.opentripmap.com/0.1/en/places/geoname?name=\(name!)&country=\(code!)&apikey=5ae2e3f221c38a28845f05b62f0092f270a88190119b3678a057dd4a".lowercased()
+        do{
+            Task{
+                var data = try! await ClientService().fetchData(uri:url)
+                var json = try! JSONSerialization.jsonObject(with: data)
+                print(json)
+                
+                let City = try! JSONDecoder().decode(CityModel.self,from: data)
+                MyCity = City
+            }
+        }catch{
+            print(error)
+        }
+    }
+    
+    
+    
+    private let APIKEY = "5ae2e3f221c38a28845f05b62f0092f270a88190119b3678a057dd4a"
+    
+    @IBAction func Button2_SightsInfo(_ sender: Any) {
+        Task{
+            await Button2_Click()
+        }
+    }
+    
+    private func Button2_Click() async {
+        var lat = MyCity!.CityLat
+        var lon = MyCity!.CityLon
+        var radius = 30*1000 // 30km or 30,000m
+        var url = "https://api.opentripmap.com/0.1/en/places/radius?radius=\(radius)&lon=\(lon)&lat=\(lat)&apikey=\(APIKEY)".lowercased()
+        do{
+            Task{
+                var data = try! await ClientService().fetchData(uri: url)
+                var json = try! JSONSerialization.jsonObject(with: data)
+                print(json)
+                sights = try! JSONDecoder().decode(SightCollection.self,from: data)
+                
+            }
+        }catch{
+            print(error)
+        }
+    }
+    
+    private var sights: SightCollection?
+    
+    @IBAction func Button3_DetailedSightesInfo(_ sender: Any){
+        //----------------MULTIPLE-TEST----------------//
+            var attempts = 0
+            sights?.features.forEach({ sight in
+            var xid = sight.properties.xid
+            var url = "https://api.opentripmap.com/0.1/en/places/xid/\(xid)?apikey=5ae2e3f221c38a28845f05b62f0092f270a88190119b3678a057dd4a"
+            do{
+                var task = Task{
+                    var data = try! await ClientService().fetchData(uri: url)
+                    var json = try! JSONSerialization.jsonObject(with: data)
+                    print(json)
+                    let mysight = try! JSONDecoder().decode(Sight.self, from: data)
+                    attempts += 1
+                    print("attempt #\(attempts)")
+                    print(mysight.name)
+                    
+                    if(attempts % 9 == 0 ){
+                        print("delay start")
+                        sleep(60)
+                        print("delay end")
+                    }
+                }
+                
+            }catch{
+                print(error)
+                print("attempt #\(attempts) fired the error")
+            }
+        })
+            
+        
+        
+        //----------------MULTIPLE-TEST----------------//
+        
+        
+        
+        
+        //----------------UNIT-TEST---------------------//
+//        let xid = "N1473999382" //"Q4092958"
+//        var url = "https://api.opentripmap.com/0.1/en/places/xid/\(xid)?apikey=5ae2e3f221c38a28845f05b62f0092f270a88190119b3678a057dd4a"
+//        do{
+//            Task{
+//                var data = try! await ClientService().fetchData(uri: url)
+//                var json = try! JSONSerialization.jsonObject(with: data)
+//                print(json)
+//                let sight = try! JSONDecoder().decode(Sight.self, from: data)
+//            }
+//        }catch{
+//            print(error)
+//        }
+        
+        //----------------UNIT-TEST---------------------//
+    }
+    
+    
+    private func DelayThread(sec: Double) async {
+        try! await Task.sleep(nanoseconds: UInt64(sec * Double(NSEC_PER_SEC)))
+    }
+    
 }
 
